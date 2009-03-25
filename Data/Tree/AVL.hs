9ba5593bed5799667cc165fe7e51c21660741f1f
Brendan Hickey <brendan@bhickey.net>
2009-03-24 23:11:31 -0400

    Restructured the interface of AVL trees to conform to Data.Map. (I hope) Remove actually works. Simplified some code. I still need 'toList' and 'toAscList' and so forth. Two hours to Geneva. 48 minutes of battery left.

--- Data/Tree/AVL.hs
+++ Data/Tree/AVL.hs
@@ -4,17 +4,19 @@
 --
 
 module Data.Tree.AVL
---(AVLTree, head, tail, singleton, empty, null, fromList, fromAscList, fromList, toAscList, insert, lookup, (!!), size) 
+(AVLTree, head, tail, singleton, empty, null, fromList, insert, delete, lookup, (!!), size) 
 where
 
-import Prelude hiding (head, tail, (!!))
+import Prelude hiding (head, tail, (!!), lookup, null)
+import Data.Maybe
+import Control.Monad.ST
 
 data AVLTree k v =
     Leaf
   | AVLTree k v Int Int (AVLTree k v) (AVLTree k v) deriving (Ord, Eq, Show)
 
-singleton :: (Ord k) => (k,v) -> AVLTree k v
-singleton (k,v) = AVLTree k v 1 1 Leaf Leaf
+singleton :: (Ord k) => k -> v -> AVLTree k v
+singleton k v = AVLTree k v 1 1 Leaf Leaf
 
 empty :: (Ord k) => AVLTree k v
 empty = Leaf
@@ -22,6 +24,14 @@ empty = Leaf
 null :: AVLTree k v -> Bool
 null Leaf = True
 null _    = False
+
+head :: (Ord k) => AVLTree k v -> v
+head Leaf = error "took the head of an empty tree"
+head (AVLTree _ v _ _ _ _) = v
+
+tail :: (Ord k) => AVLTree k v -> AVLTree k v
+tail Leaf = error "took the tail of an empty tree"
+tail t@(AVLTree k _ _ _ _ _) = delete k t
 
 size :: AVLTree k v -> Int
 size Leaf = 0
@@ -37,6 +47,23 @@ findHeight a b = 1 + max (height a) (height b)
 findSize :: AVLTree k v -> AVLTree k v -> Int
 findSize a b = 1 + size a + size b
 
+balance :: (Ord k) => AVLTree k v -> AVLTree k v
+balance Leaf = Leaf
+balance t@(AVLTree k v _ _ l r) =
+  if (abs $ (height l) - (height r)) < 2
+    then t
+  else if (height l) < (height r)
+       then case r of
+              Leaf -> error "cannot promote a leaf" -- this should never happen
+              (AVLTree k1 v1 _ _ l1 r1) -> 
+                let child = (AVLTree k v (findSize l l1) (findHeight l l1) l l1) in
+                  (AVLTree k1 v1 (findSize child r1) (findHeight child r1) child r1)  
+        else case l of
+              Leaf -> error "cannot promote a leaf"
+              (AVLTree k1 v1 _ _ l1 r1) -> 
+                let child = (AVLTree k v (findSize r1 r) (findHeight r1 r) r1 r) in
+                  (AVLTree k1 v1 (findSize l1 child) (findHeight l1 child) l1 child)
+
 (!!) :: (Ord k) => AVLTree k v -> Int -> (k,v)
 (!!) Leaf _ = error "index out of bounds"
 (!!) (AVLTree k v d _ l r) n =
@@ -50,84 +77,57 @@ findSize a b = 1 + size a + size b
            then l !! n
            else r !! (n - l' - 1)
 
-insert :: (Ord k) => AVLTree k v -> (k,v) -> AVLTree k v
-insert Leaf (k,v) = singleton (k,v)
-insert t@(AVLTree k1 v1 s h l r) (k,v) =
+lookup :: (Ord k) => k -> AVLTree k v -> Maybe v
+lookup _ Leaf = Nothing
+lookup k' t@(AVLTree k v _ _ l r) =
+  if k == k'
+  then Just v
+  else if k < k'
+       then lookup k' l
+       else lookup k' r
+
+insert :: (Ord k) => k -> v -> AVLTree k v -> AVLTree k v
+insert k v Leaf = singleton k v
+insert k v t@(AVLTree k1 v1 s h l r) =
   if k <= k1
-  then let l' = insert l (k,v) in
+  then let l' = insert k v l in
     balance (AVLTree k1 v1 (s + 1) (findHeight l' r) l' r)
-  else let r' = insert r (k,v) in
+  else let r' = insert k v r in
     balance (AVLTree k1 v1 (s + 1) (findHeight l r') l r')
 
-{-
-remove :: (Ord k) => AVLTree k v -> k -> AVLTree k v
-remove Leaf _ = Leaf
-remove t@(AVLTree k1 _ _ _ Leaf Leaf) k = if k == k1 then Leaf else t
-remove t@(AVLTree k1 v1 s h l r) k =
+delete :: (Ord k) => k -> AVLTree k v -> AVLTree k v
+delete _ Leaf = Leaf
+delete k t@(AVLTree k1 _ _ _ Leaf Leaf) = if k == k1 then Leaf else t
+delete k t@(AVLTree k1 v1 _ _ l r) =
  if k == k1
- then (remove' l)
+ then case t of
+        (AVLTree _ _ _ _ Leaf r) ->
+          let ((k', v'), r') = getLeft r in
+            balance (AVLTree k' v' (findSize Leaf r') (findHeight Leaf r') Leaf r')
+        (AVLTree _ _ _ _ l r ) ->
+          let ((k', v'), l') = getRight l in
+            balance (AVLTree k' v' (findSize l' r) (findHeight l' r) l' r)
  else if k < k1
-      then balance (AVLTree k1 v1 0 0 (remove l k) r)
-      else balance (AVLTree k1 v1 0 0 l (remove r k))
- 
-remove' :: (Ord k) => AVLTree k v -> AVLTree k v
-remove' t = t
--}
-
-balance :: (Ord k) => AVLTree k v -> AVLTree k v
-balance Leaf = Leaf
-balance t@(AVLTree k v _ _ l r) =
-  if (abs $ (height l) - (height r)) < 2
-    then (AVLTree k v (findSize l r) (findHeight l r) l r) -- this avoids the nonsense of tracking this information pre-balance
-  else if (height l) < (height r)
-       then case r of
-              Leaf -> error "cannot promote a leaf" -- this should never happen
-              (AVLTree k1 v1 _ _ l1 r1) -> 
-                let child = (AVLTree k v (findSize l l1) (findHeight l l1) l l1) in
-                  (AVLTree k1 v1 (findSize child r1) (findHeight child r1) child r1)  
-        else case l of
-              Leaf -> error "cannot promote a leaf"
-              (AVLTree k1 v1 _ _ l1 r1) -> 
-                let child = (AVLTree k v (findSize r1 r) (findHeight r1 r) r1 r) in
-                  (AVLTree k1 v1 (findSize l1 child) (findHeight l1 child) l1 child)
-
-head :: (Ord k) => AVLTree k v -> (k,v)
-head Leaf = error "took the head of an empty tree"
-head (AVLTree k v _ _ _ _) = (k,v)
-
-left :: (Ord k) => AVLTree k v -> AVLTree k v
-left Leaf = error "cannot traverse a leaf"
-left (AVLTree _ _ _ _ l _) = l
-
-right :: (Ord k) => AVLTree k v -> AVLTree k v
-right Leaf = right Leaf
-right (AVLTree _ _ _ _ _ r) = r
-
-promotePrevious :: (Ord k) => AVLTree k v -> AVLTree k v
-promotePrevious Leaf = error "unable to promote previous of Leaf"
-promotePrevious l@(AVLTree _ _ _ _ Leaf r) = promoteNext l
-promotePrevious l@(AVLTree _ _ _ _ t _) = snd $ getRight t
-
-promoteNext :: (Ord k) => AVLTree k v -> AVLTree k v
-promoteNext Leaf = error "unable to promote previous of Leaf"
-promoteNext (AVLTree _ _ _ _ _ Leaf) = Leaf
-promoteNext (AVLTree _ _ _ _ _ t) = snd $ getLeft t
+      then let l' = delete k l in
+             balance (AVLTree k1 v1 (findSize l' r) (findHeight l' r) l' r)
+      else let r' = delete k r in
+             balance (AVLTree k1 v1 (findSize l r') (findHeight l r') l r')
 
 getRight :: (Ord k) => AVLTree k v -> ((k,v), AVLTree k v)
 getRight t@(AVLTree k v s h Leaf Leaf) = ((k,v), Leaf)
 getRight t@(AVLTree k v s h l Leaf) = ((k,v), l)
 getRight t@(AVLTree k v s h l r) = 
   case getRight r of
-    (p, t2) -> (p, balance (AVLTree k v 0 0 l t2))
+    (p, t2) -> (p, balance (AVLTree k v (findSize l t2) (findHeight l t2) l t2))
 
 getLeft :: (Ord k) => AVLTree k v -> ((k,v), AVLTree k v)
 getLeft t@(AVLTree k v s h Leaf Leaf) = ((k,v), Leaf)
 getLeft t@(AVLTree k v s h Leaf r) = ((k,v), r)
 getLeft t@(AVLTree k v s h l r) = 
   case getLeft r of
-    (p, t2) -> (p, (AVLTree k v 0 0 t2 r))
+    (p, t2) -> (p, (AVLTree k v (findSize r t2) (findHeight r t2) t2 r))
 
 fromList :: (Ord k) => [(k,v)] -> AVLTree k v
 fromList [] = Leaf
-fromList (hd:[]) = singleton hd
-fromList (hd:tl) = insert (fromList tl) hd
+fromList ((k,v):[]) = singleton k v
+fromList ((k,v):tl) = insert k v (fromList tl)
